04a3290a5c37e6ff2297d4ae10066a6dcb572d25
hetvirana <hetvirana2210@gmail.com>
2023-05-05 01:34:40 +0530

    Construction

---
+++ construction/js/custom.js
@@ -0,0 +1,30 @@
+
+(function(jQuery) {
+    "use strict";
+    jQuery(window).on('load', function(e) {
+    // var Scrollbar = window.Scrollbar;
+    // console.log('Scrollbar');
+   
+    /*------------------------
+    Search Button
+    --------------------------*/
+    // jQuery('#pt-seacrh-btn').on('click', function() {
+    // jQuery('.pt-search-form').slideToggle();
+    // jQuery('.pt-search-form').toggleClass('pt-form-show');
+    // if (jQuery('.pt-search-form').hasClass("pt-form-show")) {
+    // jQuery(this).html('<i class="fa fa-times"></i>');
+    // } else {
+    // jQuery(this).html('<i class="fa fa-search"></i>');
+    // }
+    // });
+   
+    /*----------------
+    Counter
+    ---------------------*/
+    jQuery('.timer').countTo();
+   
+            
+    
+    });
+    
+    });
